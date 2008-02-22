71aeb1e45a02c36d7a4476abbca5b1dd2e2fbcbc
Patrick Aboyoun <unknown>
2008-02-22 00:35:14 +0000

    In locf2func, added some infrastructure to automatically increase the maximum number of bins to use in locfit calls in order to support KLD.matrix() calls.
    In the vignette, cleaned up text body, added a title, and and added section headings.
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/bioDist@30096 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/locf2func.R
+++ R/locf2func.R
@@ -1,6 +1,11 @@
-locf2func <- function(x, ...) {
-    require(locfit) || stop("can only be used if locfit library is available")
-    l = locfit(~x, ...)
+locf2func <- function(x, ..., maxk = 100) {
+    if(!require(locfit))
+		stop("can only be used if locfit library is available")
+    for(i in 1:10) {
+		l <- try(locfit(~x, ..., maxk = i * maxk))
+		if(!is(l, "try-error"))
+			break;
+	}
     den1 <- preplot(l,...)
     xvals <- den1$xev[[1]]
     yvals <- den1$trans(den1$fit)
